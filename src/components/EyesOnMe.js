2d0aaa3a4134781c389639cfcbc64be76f0b73cd
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-01 14:17:09 -0400

    completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react"
+
+const focusEvent = () => console.log("Good!")
+const blurEvent = () => console.log("Hey! Eyes on me!")
+
+const EyesOnMe = () => (
+  <button 
+    onFocus={focusEvent} 
+    onBlur={blurEvent}>
+      Eyes on me
+  </button>
+)
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
-// Code Keypad Component Here
+import React from "react"
+
+const enteringPassword = () => console.log("Entering password...")
+
+const Keypad = () => (
+  <input type="password" onChange={enteringPassword}/>
+)
+
+export default Keypad
